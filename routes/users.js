ba9f829ff6d72c5bcf6dde45587b89b4cdab8b18
f2015113 <dixitshivang96@gmail.com>
2019-01-15 19:12:58 +0530

    Password hashing using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -14,14 +16,11 @@ router.post('/' , async (req,res) => {
     if(user) {
         return res.status(400).send('User already registered');
     }
-    user = new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    });
-
-    await user.save();
-    res.send(user);
+    user = new User(_.pick(req.body , ['name' , 'email' , 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password , salt);
+    await user.save(); 
+    res.send(_.pick(user , ['name' , 'email' , '_id']));
 });
 
 module.exports = router;
